808dfeaa6e03c07deddae62b6a36aa5c5d476ede
Rafael <rafa.viana.moc@gmail.com>
2023-07-11 10:59:54 -0300

    Starting game styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
-
+  import {ref} from 'vue';
+  const title = ref('title');
 </script>
 
 <template>
   <div>
-    
+    <h1 :id="title">Hangman Game</h1>
   </div>
 </template>
 
 <style scoped>
-
+  #title{
+    color: var(--color-text-title);
+  }
 </style>
