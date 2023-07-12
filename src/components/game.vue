aa6b42e2c436b09e2acf54814a016373892c3e8e
Rafael <rafa.viana.moc@gmail.com>
2023-07-12 00:20:20 -0300

    Rendering letters in screen

--- src/components/game.vue
+++ src/components/game.vue
@@ -1,9 +1,20 @@
 <script>
     import Letter from './letter.vue';
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
+    "l", "m", "n", "o", "p", "q", "r", "s",
+     "t", "u", "v", "w", "x", "y", "z"];
+
 </script>
 
 <template>
+    <main>
+        <section>
 
+        </section>
+        <section>
+            <Letter v-for="letter in alphabet" />
+        </section>
+    </main>
 </template>
 
 <style>
